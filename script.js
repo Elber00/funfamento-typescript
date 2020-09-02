bc24f1f34dcb85950e118cdd051307c6f7402894
Elber Pittner <69132@PROWAY.TREINA>
2020-09-01 22:09:48 -0300

    conteudo

---
+++ script.js
@@ -0,0 +1,69 @@
+
+            var x =5;
+            var y =9;
+
+            var somar =(x + y);
+            var sublitrair = (x - y);
+            var dividir = ( x/ y);
+            var multiplicare = (x * y);
+            alert( " soma é : " + somar);
+            alert( " sublitar é: " + sublitrair);
+            alert( "multiplicar é" + multiplicare);
+            alert( "dividir é " + dividir);
+
+          var ternario = somar >=0 ?  alert("é maior") : alert ("é menor ");
+
+         for (var i=0; i< 10; i++ ){
+             console.log ("o valor de i ->" + i);
+         } 
+
+         console.
+              alert ("o valor da soma e: " + a+b);
+              somando(1.3);
+              const varTexto : number = 10;
+              const varTextoOutro : string = 'novo';
+              const varjunto = varTexto + varTextoOutro;
+              
+              console.log boolean = false
+              
+              let meuBoolean: boolean = false,
+              
+              //o number é um ponto flutuante,
+              //hexa octal e binario
+              let meuOctal: number = 0b11;
+              alert(meuBinario);
+              
+              let meuHexa: number = 0xab13;
+              alert(meuHexa)
+              
+              let texto: string= "ola typescript";
+              alert(texto);
+              
+              let logradouro: string = "Rua 7 de setembro";
+              let numero :  string = "1238";
+              let cidade : string = "Blumenau"
+              let estado : string "sc"
+              alert ("endereço : + logradouro + "," + numero + "." + cidade ","_ + estado);
+              
+              // Interpolação de string
+              
+              alert(`endereço: ${logradouro}, ${numero}. ${cidade} - ${estado} `);
+              
+              // tipo  array em typescrip
+              let meuArray: number[] = [1,2,3,4];
+              alert (meuArray)
+              
+              // pode ser feito com o <> ira ter o mesmo efeito com isso fica array
+              let meuArray2: Array<number> = [1,2,3,4] ;
+              
+              //operador com tuplas
+              //exemplo de tuplas sao os dicionarios
+              /*palavra(string) -> signigicado(string) */
+              let campeaoBrasileiro: [string, number] = ["palmeiras",2005]
+              
+              alert (campeaoBrasileiro[0] + " - " + campeaoBrasileiro[1] );
+              
+              let campeaoBrasileiro: [ string,number] = ["palavra, 2016"];
+              campeaoBrasileiro[0] = "teste";
+              
+              alert (campeaoBrasileiro); 
